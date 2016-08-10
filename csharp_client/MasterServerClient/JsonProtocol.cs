8dd526913198abd466c14bd4bc0993afb9f15123
Frank Versnel <f.versnel@gmail.com>
2016-08-10 10:51:26 +0200

    Updated IP endpoint JSON protocol.

--- csharp_client/MasterServerClient/JsonProtocol.cs
+++ csharp_client/MasterServerClient/JsonProtocol.cs
@@ -1,37 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Globalization;
 using System.Net;
 using Newtonsoft.Json;
 
 namespace RamjetAnvil.Padrone.Client {
     public static class JsonProtocol {
-
-        public class IPEndpointConverter : JsonConverter {
+        public class ClientSecretConverter : JsonConverter {
             public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) {
-                var ipEndpoint = (IPEndPoint)value;
-                serializer.Serialize(writer, new Dictionary<string, object> {
-                    {"address", ipEndpoint.Address.ToString()},
-                    {"port", ipEndpoint.Port}
-                });
+                var sessionId = (ClientSecret) value;
+                serializer.Serialize(writer, sessionId.Value);
             }
 
-            public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer) {
-                var ipEndpoint = serializer.Deserialize<IDictionary<string, object>>(reader);
-                return new IPEndPoint(IPAddress.Parse(Convert.ToString(ipEndpoint["address"])), Convert.ToInt32(ipEndpoint["port"]));
+            public override object ReadJson(JsonReader reader, Type objectType, object existingValue,
+                JsonSerializer serializer) {
+                var sessionId = serializer.Deserialize<string>(reader);
+                return new ClientSecret(sessionId);
             }
 
             public override bool CanConvert(Type objectType) {
-                return objectType == typeof (IPEndPoint);
+                return objectType == typeof (ClientSecret);
             }
         }
 
         public class ClientSessionIdConverter : JsonConverter {
             public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) {
-                var sessionId = (ClientSessionId)value;
+                var sessionId = (ClientSessionId) value;
                 serializer.Serialize(writer, sessionId.Value);
             }
 
-            public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer) {
+            public override object ReadJson(JsonReader reader, Type objectType, object existingValue,
+                JsonSerializer serializer) {
                 var sessionId = serializer.Deserialize<string>(reader);
                 return new ClientSessionId(sessionId);
             }
@@ -41,19 +39,40 @@ namespace RamjetAnvil.Padrone.Client {
             }
         }
 
-        public class ClientSecretConverter : JsonConverter {
+        public class IPEndpointConverter : JsonConverter {
             public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) {
-                var sessionId = (ClientSecret)value;
-                serializer.Serialize(writer, sessionId.Value);
+                var ipEndpoint = (IPEndPoint) value;
+                serializer.Serialize(writer, ipEndpoint.ToString());
             }
 
-            public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer) {
-                var sessionId = serializer.Deserialize<string>(reader);
-                return new ClientSecret(sessionId);
+            public override object ReadJson(JsonReader reader, Type objectType, object existingValue,
+                JsonSerializer serializer) {
+                var ipEndpointStr = serializer.Deserialize<string>(reader);
+                return ParseIpEndPoint(ipEndpointStr);
             }
 
             public override bool CanConvert(Type objectType) {
-                return objectType == typeof (ClientSecret);
+                return objectType == typeof (IPEndPoint);
+            }
+
+            private static IPEndPoint ParseIpEndPoint(string endPoint) {
+                string[] ep = endPoint.Split(':');
+                if (ep.Length < 2) throw new FormatException("Invalid endpoint format");
+                IPAddress ip;
+                if (ep.Length > 2) {
+                    if (!IPAddress.TryParse(string.Join(":", ep, 0, ep.Length - 1), out ip)) {
+                        throw new FormatException("Invalid ip-adress");
+                    }
+                } else {
+                    if (!IPAddress.TryParse(ep[0], out ip)) {
+                        throw new FormatException("Invalid ip-adress");
+                    }
+                }
+                int port;
+                if (!int.TryParse(ep[ep.Length - 1], NumberStyles.None, NumberFormatInfo.CurrentInfo, out port)) {
+                    throw new FormatException("Invalid port");
+                }
+                return new IPEndPoint(ip, port);
             }
         }
     }
